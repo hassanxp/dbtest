2e765c8d9eb1b84ea3fb57f981dde32135f0a312
Hassan Siddiqui <hassans@astro.princeton.edu>
2019-11-22 17:32:22 -0500

    added comments from CPL

--- spstables.py
+++ spstables.py
@@ -7,38 +7,56 @@ Base = declarative_base()
 
 
 # From Yabe-san's core schema
-class pfi_visit(Base):
-    __tablename__ = 'pfi_visit'
+# class pfi_visit(Base):
+#     __tablename__ = 'pfi_visit'
 
-    pfi_visit_id = Column(Integer, primary_key=True, unique=True, autoincrement=False)
-    pfi_visit_description = Column(String)
+#     pfi_visit_id = Column(Integer, primary_key=True, unique=True, autoincrement=False)
+#     pfi_visit_description = Column(String)
 
-    def __init__(self, pfi_visit_id, pfi_visit_description):
-        self.pfi_visit_id = pfi_visit_id
-        self.pfi_visit_description = pfi_visit_description
-
-
-class pfs_site(Base):
-    __tablename__ = 'pfs_site'
-
-    pfs_site_id = Column(Integer, primary_key=True, unique=True, autoincrement=False)
-    site_description = Column(String)
-
-    def __init__(self, pfs_site_id, site_description):
-        self.pfs_site_id = pfs_site_id
-        self.site_description = site_description
+#     def __init__(self, pfi_visit_id, pfi_visit_description):
+#         self.pfi_visit_id = pfi_visit_id
+#         self.pfi_visit_description = pfi_visit_description
 
 
-class sps_arm(Base):
-    __tablename__ = 'sps_arm'
+# class pfs_site(Base):
+#     __tablename__ = 'pfs_site'
 
-    sps_arm_id = Column(String, primary_key=True, unique=True, autoincrement=False)
-    sps_arm_description = Column(String)
+#     pfs_site_id = Column(Integer, primary_key=True, unique=True, autoincrement=False)
+#     site_description = Column(String)
 
-    def __init__(self, sps_arm_id, sps_arm_description):
-        self.sps_arm_id = sps_arm_id
-        self.sps_arm_description = sps_arm_description
+#     def __init__(self, pfs_site_id, site_description):
+#         self.pfs_site_id = pfs_site_id
+#         self.site_description = site_description
 
+
+# class sps_arm(Base):
+#     __tablename__ = 'sps_arm'
+
+#     sps_arm_id = Column(String, primary_key=True, unique=True, autoincrement=False)
+#     sps_arm_description = Column(String)
+
+#     def __init__(self, sps_arm_id, sps_arm_description):
+#         self.sps_arm_id = sps_arm_id
+#         self.sps_arm_description = sps_arm_description
+
+
+# class sps_anomalies(Base):
+#     __tablename__ = 'sps_anomalies'
+
+#     anomaly_id = Column(Integer, primary_key=True, autoincrement=False)
+#     pfi_visit_id = Column(Integer, ForeignKey('pfi_visit.pfi_visit_id'))
+#     pfs_site_id = Column(Integer, ForeignKey('pfs_site.pfs_site_id'))
+#     sps_module = Column(Integer)
+#     sps_arm_id = Column(String, ForeignKey('sps_arm.sps_arm_id'))
+#     description = Column(String)
+
+#     def __init__(self, anomaly_id, visit_id, pfs_site_id, sps_module, sps_arm_id, description):
+#         self.anomaly_id = anomaly_id
+#         self.visit = visit_id
+#         self.pfs_site_id = pfs_site_id
+#         self.sps_module = sps_module
+#         self.sps_arm_id = sps_arm_id
+#         self.description = description
 
 class visit_set(Base):
     __tablename__ = 'visit_set'
@@ -51,10 +69,10 @@ class visit_set(Base):
         self.visit_set_id = visit_set_id
 
 
-class visit_info(Base):
-    __tablename__ = 'visit_info'
+class sps_visit(Base):
+    __tablename__ = 'sps_visit'
 
-    pfi_visit_id = Column(Integer, ForeignKey('pfi_visit.pfi_visit_id'), primary_key=True)
+    pfi_visit_id = Column(Integer, primary_key=True)  # Link to pfi_visit_id later
     visit_set_id = Column(Integer, ForeignKey('visit_set.visit_set_id'))
     visit_type = Column(String) # BIAS, DFLAT etc - another table defining them?
 
@@ -69,7 +87,7 @@ class sps_annotation(Base):
 
     sps_annotation_id = Column(Integer, primary_key=True, autoincrement=False)
     visit_set_id = Column(Integer, ForeignKey('visit_set.visit_set_id'))
-    pfi_visit_id = Column(Integer, ForeignKey('pfi_visit.pfi_visit_id'))
+    pfi_visit_id = Column(Integer) # Connect to pfi_visit_id table later
     comment = Column(String)
     anomaly = Column(String)
 
@@ -86,8 +104,8 @@ class sps_camera(Base):
 
     sps_camera_id = Column(Integer, primary_key=True, autoincrement=False)
     visit_set_id = Column(Integer, ForeignKey('visit_set.visit_set_id'))
-    sps_module = Column(Integer)
-    sps_arm_id = Column(String, ForeignKey('sps_arm.sps_arm_id'))
+    sps_module = Column(Integer) # Foreign key to module table?
+    sps_arm_id = Column(String) # Foreign key to arm table?
 
     def __init__(self, camera_id, visit_set_id, sps_module, sps_arm_id):
         self.sps_camera_id = camera_id
@@ -96,31 +114,12 @@ class sps_camera(Base):
         self.sps_arm_id = sps_arm_id
 
 
-class sps_anomalies(Base):
-    __tablename__ = 'sps_anomalies'
-
-    anomaly_id = Column(Integer, primary_key=True, autoincrement=False)
-    pfi_visit_id = Column(Integer, ForeignKey('pfi_visit.pfi_visit_id'))
-    pfs_site_id = Column(Integer, ForeignKey('pfs_site.pfs_site_id'))
-    sps_module = Column(Integer)
-    sps_arm_id = Column(String, ForeignKey('sps_arm.sps_arm_id'))
-    description = Column(String)
-
-    def __init__(self, anomaly_id, visit_id, pfs_site_id, sps_module, sps_arm_id, description):
-        self.anomaly_id = anomaly_id
-        self.visit = visit_id
-        self.pfs_site_id = pfs_site_id
-        self.sps_module = sps_module
-        self.sps_arm_id = sps_arm_id
-        self.description = description
-
-
 class processing_status(Base):
     __tablename__ = 'processing_status'
 
     status_id = Column(Integer, primary_key=True, autoincrement=False)
     visit_set_id = Column(Integer, ForeignKey('visit_set.visit_set_id'))
-    pfi_visit_id = Column(Integer, ForeignKey('pfi_visit.pfi_visit_id'))
+    pfi_visit_id = Column(Integer) # Link to pfi_visit table later
     data_ok = Column(Boolean)
 
     def __init__(self, status_id, visit_set_id, pfi_visit_id, data_ok):
